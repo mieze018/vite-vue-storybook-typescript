e1f8962fee72ca7ed9eaceba6f250389a804018b
Ayu Nakata <mieze018@users.noreply.github.com>
2022-04-19 01:03:52 +0900

    「Vue3でVue Router4を使う方法」チュートリアル実行

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,18 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue';
+
 </script>
 
 <template>
+
+  <AppHeader />
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div>
+  <router-view></router-view>
+</div>
 </template>
 
 <style>
---
+++ src/pages/Page2.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Page2です</h1>
+  </div>
+</template>
+ 
+<script lang="ts">
+export default {
+ 
+}
+</script>
+ 
+<style>
+div {
+  width: 100%;
+  text-align: center;
+}
+</style>
