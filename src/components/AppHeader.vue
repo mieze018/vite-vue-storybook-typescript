23155ba23f7e6d56177d51e114cfba2c54b1da04
Ayu Nakata <mieze018@users.noreply.github.com>
2022-04-23 20:12:04 +0900

    Update AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,29 +2,26 @@
   <header>
     <h1>Vue Router Test</h1>
     <nav>
-      <router-link
-        v-for="list in navList" :key="list.to"
-        :to="list.to" 
-      >
-        {{list.title}}
+      <router-link v-for="list in navList" :key="list.to" :to="list.to">
+        {{ list.title }}
       </router-link>
     </nav>
   </header>
 </template>
- 
+
 <script lang="ts">
 export default {
-  data(){
-    return{
-      navList:[
-        {title:"Page1", to:"/Page1"},
-        {title:"Page2", to:"/Page2"},
-      ]
-    }
-  }
-}
+  data() {
+    return {
+      navList: [
+        { title: "Page1", to: "/Page1" },
+        { title: "Page2", to: "/Page2" },
+      ],
+    };
+  },
+};
 </script>
- 
+
 <style>
 header {
   display: flex;
@@ -35,7 +32,7 @@ header {
 h1 {
   padding: 10px;
 }
-nav a{
+nav a {
   display: inline-block;
   padding: 20px 30px;
 }
